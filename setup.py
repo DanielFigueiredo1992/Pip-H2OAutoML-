415171f9f2053c452433e09f6dff6a796363288f
DanielFigueiredo1992 <124630116+DanielFigueiredo1992@users.noreply.github.com>
2024-05-14 04:01:55 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+import subprocess
+
+# Comando a ser executado
+command = 'apt install python3.10-venv && git clone https://github.com/h2oai/wave-h2o-automl.git && cd wave-h2o-automl && make setup && source venv/bin/activate'
+
+# Iniciar o subprocesso e capturar a saída
+process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+
+# Capturar e imprimir a saída em tempo real
+for line in process.stdout:
+    print(line.strip())
+
+# Esperar pelo término do processo
